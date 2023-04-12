2163df1ee6ef0bcbf0534cb65a8fb7faa7ebad3d
silvanoguel <silvanoguel@msn.com>
2023-04-12 14:25:26 +0200

    add:AppTitle created

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+    name: "AppTitle",
+    data(){
+        return {
+            text: "La mia prima app con Vite!"
+        }
+    },
+    methods: {
+
+    }
+}
+
+</script>
+
+<template>
+
+    <p> {{ text }}</p>
+
+</template>
+
+<style>
+p {
+    color: red;
+    font-size: 30px;
+}
+
+</style>
